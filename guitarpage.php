7f72e0d6ad3b993634ef383a43d924cd19589481
seandoolen <38441400+seandoolen@users.noreply.github.com>
2018-04-25 15:49:03 -0700

    added a buying button

--- guitarpage.php
+++ guitarpage.php
@@ -9,9 +9,12 @@
 
 <?php
     include 'model.php';
+    session_start();
     $id = $_GET['id'];
     $arr = $theDBA->getGuitarById($id);
+    $user = $_SESSION['user'];
     
+    echo $user;
     
         echo "<div class='con'>";
         echo "<img class='buy' src='img/$id.jpg'><br><br>";
@@ -19,12 +22,16 @@
         echo "Brand: ".$arr[0]['brand'] ."<br><br>";
         echo "Name: ".$arr[0]['name'] ."<br><br>";
         echo "Price: $".$arr[0]['price'] ."<br><br>";
-        echo "</div>";
-        if(isset($_SESSION['user']))
-            echo "<button class='buy'>Add To Cart</button>";
+        if(isset($user))
+        {
+            echo "<button class='acc' onclick='addToCart($user,$id)'>Add To Cart</button>";
+        }
         else
+        {     
             echo "<div class='warn'>You Must Have An Account To Purchase Guitars!</div><br>
              <button class='acc'onclick='login()'>Login</button><button class='acc' onclick='register()'>Register</button>";
+        }
+        echo "</div>";
 ?>
 
 <script>
@@ -37,6 +44,24 @@ function login()
 {
 		window.location.href = "login.php";
 }
+
+function addToCart($user,$id)
+{
+	alert("Called function");
+	var ajax = new XMLHttpRequest();
+	ajax.open("GET","controller.php?mode=buy&user="+$user + "&id=" +$id,true);
+	ajax.send();
+	ajax.onreadystatechange = function()
+	{
+		if(ajax.readyState == 4 && ajax.status == 200)
+		{
+			window.loaction.href = "view.php";
+		}	
+	}
+}
+
 </script>
+
 </body>
 </html>
+
