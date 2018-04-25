ee8f9f5ae4a3852be457abd2e46ee5efa8192370
seandoolen <38441400+seandoolen@users.noreply.github.com>
2018-04-25 15:48:04 -0700

    added buying and regeister

--- controller.php
+++ controller.php
@@ -5,4 +5,16 @@ if($_GET['mode'] == 'merch')
     $arr = $theDBA->getAllGuitars();
     echo json_encode($arr);
 }
-?>
+else if($_GET['mode'] == 'buy')
+{
+    $usr = $_GET['user'];
+    $prodID = $_GET['pass'];
+    $theDBA->addToCart($usr, $prodId);
+}
+else if($_GET['mode'] == 'register')
+{
+    $usr = $_GET['user'];
+    $pwd = $_GET['pass'];
+    $rt = $theDBA->createAccount($usr, $pwd);
+    echo json_encode($rt);
+}
